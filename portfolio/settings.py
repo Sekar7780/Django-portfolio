886d1720bf07e39cad24af0ccc707d04c9a955ff
Sekar7780 <sekar.gauthamraj@gmail.com>
2025-01-17 20:09:21 +0530

    Update settings and other files for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Ensure this matches your static files location
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
